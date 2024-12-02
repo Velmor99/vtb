c111345db52061b6c3b48f2497663e1ac6dcfa77
roman <wincentwangog@gmail.com>
2024-12-02 10:37:15 +0200

    vue router without file router navigation

---
+++ src/views/ProjectsPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Projects Page</h1>
+        <RouterLink to="/">Go to home page</RouterLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+    
+</script>
+
+<style scoped>
+
+</style>
